95d639e3a0d59451bafac2aa7de5d5e1465e9414
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-19 10:00:33 -0700

    update stylists controller with route to view the stylist list

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -8,6 +8,16 @@ namespace HairSalon.Controllers
 {
   public class StylistsController : Controller
   {
-    
+    private readonly HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
   }
 }

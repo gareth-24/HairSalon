e4c1255413ad98f155ff018eed653cd11ce775cc
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-19 10:37:43 -0700

    update controllers with route to add a new stylist. update model and db with new YearsEmployed property for stylists

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,6 +7,7 @@ namespace HairSalon.Models
       public int StylistId { get; set; }  //Primary Key
       public string StylistName { get; set; }
       public string Specialties { get; set; }
+      public int YearsEmployed { get; set; }
       public List<Client> Clients { get; set; }
     }
 }

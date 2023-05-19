b12b4c9665e5eff15680930c73febe9103326a26
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-19 10:59:00 -0700

    update clients controller with route to the index view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -9,6 +9,18 @@ namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
-    
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients
+                              .Include(client => client.Stylist)
+                              .ToList();
+      return View(model);
+    }
   }
 }
